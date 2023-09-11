466470cf735ed640870959c47385c013c535f456
Pramod <sharmapramod2811@gmail.com>
2023-09-11 10:09:12 +0530

    update code

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@ int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
    printf("Pramod kumar sharma");
-   printf("AUM DIA  TECHNOLOGIES");
+   printf("AUM DIA TECHNOLOGIES");
    return 0;
 }
