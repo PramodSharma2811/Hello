6362dee3aa079801ffb6d05f4e3c11231563163f
Pramod <sharmapramod2811@gmail.com>
2023-09-12 11:31:16 +0530

    some files

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   printf("Pramod kumar sharma");
-   printf("AUM DIA TECHNOLOGIES");
-   return 0;
-}
